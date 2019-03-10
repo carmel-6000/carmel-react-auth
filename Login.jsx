972f291ed5a33ee747881a898d611a80e2e2d6cb
ReutSchremer <totireut@gmail.com>
2019-03-10 14:57:58 +0200

    registration applyed

--- Login.jsx
+++ Login.jsx
@@ -2,71 +2,142 @@ import React, { Component } from 'react';
 import './Login.css';
 import Auth from './Auth';
 import { Redirect } from 'react-router';
+import ReactModal from 'react-responsive-modal';
 
-class Login extends Component{
-    
-    constructor(props){
+class Login extends Component {
+
+    constructor(props) {
         super(props);
-        this.handleLogin=this.handleLogin.bind(this);
-        this.state={
-            isLoading:false,
-            redirTo:false
+        this.handleLogin = this.handleLogin.bind(this);
+        this.state = {
+            isLoading: false,
+            redirTo: false,
+            registerModal: false
         }
 
     }
-    handleLogin(e){
+    handleLogin(e) {
         e.preventDefault();
-        
-        let email=this.refs.email.value;
-        let pw=this.refs.pw.value;
-        this.setState({isLoading:true});
-        
-        Auth.authenticate(email,pw,(isAuthenticated)=>{
 
-            this.setState({isLoading:false});
-            if (isAuthenticated===false){
+        let email = this.refs.email.value;
+        let pw = this.refs.pw.value;
+        this.setState({ isLoading: true });
+
+        Auth.authenticate(email, pw, (isAuthenticated) => {
+
+            this.setState({ isLoading: false });
+            if (isAuthenticated === false) {
                 alert("Login Failed, \n Try again");
                 return;
             }
-            if (isAuthenticated===true){
-                //{this.props.navHeader()};
-                this.setState({redirTo:'home'});
+            if (isAuthenticated === true) {
+                { this.props.navHeader() };
+                this.setState({ redirTo: '/home' });
             }
-            
+
         });
 
         return false;
     }
 
+    openRegModal = () => {
+        this.setState({ registerModal: !this.state.registerModal });
+    }
 
-    render(){
-        if (this.state.redirTo!==false){
-            return (<Redirect to={{pathname:'/',state:this.state}} />);
-        }else
-        return(
-            <div className='loginPage'>
-                <div className='loginBox'>
-                    <div className='frow'>
-                        <h3>Welcome to Carmel Admin! Who are you?</h3>
+    register = (e) => {
+        e.preventDefault();
+        let fd = new FormData(document.getElementById("registrationForm"));
+        var payload = {};
+        fd.forEach(function (value, key) {
+            payload[key] = value;
+        });
+        fetch('/api/Users', {
+            headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
+            method: "POST",
+            body: JSON.stringify(payload)
+        }).then((res => res.json()))
+            .then(res => {
+                if (!res.error) {
+                    console.log("User registered!!", res);
+                    alert("You are now a student in Creathush!")
+                    return false;
+                }
+                else {
+                    if (res.error.code)
+                        alert(res.error.message)
+                    else if (res.error.details.codes.email[0] = "uniqueness")
+                        alert("This email is alredy registered in our system.")
+
+                }
+            }).catch(error => {
+                console.log("error!!", error);
+                // alert()
+            })
+
+    }
+
+    render() {
+        console.log("this state", this.state)
+        if (this.state.redirTo != false) {
+            return (<Redirect to={{
+                pathname: '/home', state: this.state
+            }} />);
+
+        } else
+            return (
+                <div className='loginPage'>
+
+                    <div className='loginBox'>
+                        <div className='frow'>
+       
+                        </div>
+                        <form className="form" onSubmit={this.handleLogin}>
+                        <p className="mt-1">ברוכים הבאים לקריאיטוש!</p>
+                            <div className='form-group'>
+                                <input className="form-control" type='email' ref='email' placeholder='מייל' required />
+                            </div>
+                            <div className='form-group'>
+                                <input className="form-control" type='password' ref='pw' placeholder='סיסמא' required />
+                            </div>
+                            <div className='form-group'>
+                                {this.state.isLoading ?
+                                    <button className='btn btn-warning'>מתחבר...</button> :
+                                    <input type='submit' className='btn btn-warning login_input' value='היכנס' />
+                                }
+                            </div>
+                        </form>
+                        <div className='frow'>
+                            <p onClick={this.openRegModal}>לא רשומים? הירשמו עכשיו!</p>
+                            <ReactModal closeOnOverlayClick shouldCloseOnEsc showCloseIcon open={this.state.registerModal} center onClose={this.openRegModal}>
+                                <form className="form" id="registrationForm" style={{ textAlign: 'center' }} onSubmit={this.register}>
+                                    <p className="mt-3">מלאו את הפרטים הבאים</p>
+                                    <div className="form-group">
+                                        <label for="registerPrivateName">הכנס שם פרטי</label>
+                                        <input name='realm' id="registerPrivateName" className="form-control" type='text' placeholder="הכנס את שמך"></input>
+                                    </div>
+                                    <div className="form-group">
+                                        <label for="registerEmail">הכנס כתובת מייל</label>
+                                        <input name='email' id="registerEmail" className="form-control" type='email' required placeholder={"example@gmail.com"}></input>
+                                    </div>
+                                    <div className="form-group">
+                                        <label for="registerUserName">הכנס שם משתמש</label>
+                                        <input name='username' id="registerUserName" className="form-control" type='text' placeholder="הכנס שם משתמש"></input>
+                                    </div>
+                                    <div className="form-group">
+                                        <label for="registerPasswd">הכנס סיסמא</label>
+                                        <input required name='password' id="registerPasswd" className="form-control" type='password' placeholder="הכנס סיסמא"></input>
+                                    </div>
+                                    <button className='btn btn-warning' type='submit'>הירשם!</button>
+                                </form>
+                            </ReactModal>
+                        </div>
+
+
                     </div>
-                    <form onSubmit={this.handleLogin}>
-                        <div className='frow'>
-                            <input type='email' ref='email' placeholder='Email' required/>
-                        </div>
-                        <div className='frow'>
-                            <input type='password' ref='pw' placeholder='Password' required/>
-                        </div>
-                        <div className='frow'>
-                            {this.state.isLoading?
-                                <button className='btn btn-warning'>Hold on...</button> : 
-                                <input type='submit' className='btn btn-warning' value='SEND' />
-                            }
-                        </div>
-                    </form>
                 </div>
-            </div>
             )
-    }    
+    }
+
 }
 
 export default Login;
