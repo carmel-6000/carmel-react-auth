cb3d00342f217174dce0080e5b9d3705e2d6f2be
ReutSchremer <totireut@gmail.com>
2019-03-25 11:02:36 +0200

    auth fetch + payload, customUser

--- Auth.js
+++ Auth.js
@@ -1,7 +1,4 @@
-import React from "react";
-import { Redirect } from 'react-router';
-
-const Auth={
+const Auth = {
 
   _isAuthenticated:false,
   
@@ -13,8 +10,10 @@ const Auth={
     return this._isAuthenticated;
 
   },
-  authFetch(url) {
-
+  getUserId() {
+    return { userId: localStorage.getItem("userId"), userRole: localStorage.getItem("userRole") };
+  },
+  authFetch(url, payload = null) {
     let accessToken = localStorage.getItem('accessToken');
     console.log("authFetch given accessToken:", accessToken);
     if (accessToken === null) {
@@ -26,14 +25,20 @@ const Auth={
       } else {
         url += "?access_token=" + accessToken;
       }
-      return fetch(url);
+      if (payload)
+        return fetch(url, payload);
+      else return fetch(url);
     }
+  },
+  getRoutingCode() {
+    return localStorage.getItem('com');
   },
 
   authenticate(email,pw,cb){
 
-    fetch('/api/Users/login', {method: 'POST',headers: {'Accept': 'application/json','Content-Type': 'application/json'},
-        body: JSON.stringify({email: email, password: pw})
+    fetch('/api/CustomUsers/login/', {
+      method: 'POST', headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email: email, password: pw })
     })
 
     .then(response=>{return response.json()}).then(res=> {
@@ -41,22 +46,30 @@ const Auth={
         console.log("res",res);
         if (res.error){
 
-            this._isAuthenticated=false;
-            localStorage.setItem('accessToken','');
-            return cb(false);
-        }else{
+          this._isAuthenticated = false;
+          localStorage.setItem('accessToken', '');
+          localStorage.setItem('userId', '');
+          localStorage.setItem('com', '')
+          return cb(false);
+        } else {
 
-            this._isAuthenticated=true;    
-            localStorage.setItem('accessToken',res.id);
-            return cb(true)
+          this._isAuthenticated = true;
+          localStorage.setItem('accessToken', res.id);
+          localStorage.setItem('userId', res.userId);
+          localStorage.setItem('com', res.compArr)
+          return cb(true)
         }
         
     }); 
   },
-  logout(cb){
-
-    localStorage.removeItem('accessToken','');
-    this._isAuthenticated=false;
+  logout(cb) {
+    console.log("log out!")
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('userId');
+    localStorage.removeItem('com')
+    this._isAuthenticated = false;
+    if (cb)
+      cb();
     return;
   }
 
