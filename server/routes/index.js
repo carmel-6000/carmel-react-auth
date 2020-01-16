d2d2c921ab3964faa4a90761dcf49b733afefbc4
shirarab <broomonster@gmail.com>
2020-01-16 21:09:29 +0200

    regular consolelogs, no debug necessary

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,10 @@
-const logAuth = require('debug')('model:auth');
 
 module.exports = function (app) {
     app.get('/', function (req, res, next) {
-        logAuth("auth middleware for / is launched");
+        console.log("auth middleware for / is launched");
         let cookieVal = (req.accessToken && req.accessToken.id) ? req.accessToken.id : '';
         if (!cookieVal) {
-            logAuth("AccessToken is not valid, deleting cookies...");
+            console.log("AccessToken is not valid, deleting cookies...");
             let cookieKeys = ['access_token', 'kl', 'klo', 'klk', 'kloo', 'olk'];
             for (let key of cookieKeys) { res.clearCookie(key); }
         }
