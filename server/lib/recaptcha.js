1e34c1e98f968a963a2f09df30dff01d3608cd3d
shirarab <broomonster@gmail.com>
2020-02-11 18:09:36 +0200

    fixed some problem

--- server/lib/recaptcha.js
+++ server/lib/recaptcha.js
@@ -1,8 +1,9 @@
 
 let request = require("request")
+const logUser = require('debug')('model:user');
 
 const verify = function (secretKey, req, cb) {
-    console.log("req.bodyb", req.body)
+    logUser("req.bodyb", req.body)
     if (!req.body.captcha) return cb({ success: false });
 
     const verifyUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${req.body.captcha}`
