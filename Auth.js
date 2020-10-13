13a961cf53274dd1051524eaca1534892fb6cdf0
sbendahan <sbendahan32@gmail.com>
2020-10-13 18:36:57 +0300

    chages required by myday

--- Auth.js
+++ Auth.js
@@ -58,7 +58,7 @@ const Auth = {
     }
   },
 
-  async superAuthFetch(url, payload = null, redirOnFailure = true) {
+  async superAuthFetch(url, payload = null, redirOnFailure = false) {
     let [res, err] = await AsyncTools.superFetch(url, payload);
     if (err && err.error && err.error.statusCode === 401 && redirOnFailure === true) {
       Auth.logout(() => window.location.href = window.location.origin); //FORCE LOGOUT.      
